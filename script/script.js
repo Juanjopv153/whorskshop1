47b315041985118f02f8c0f467801eb9f000912f
juanjopv153 <juanjopv153@gmail.com>
2022-05-20 16:58:08 -0500

    implementación del cambio de página

--- script/script.js
+++ script/script.js
@@ -1,12 +1,14 @@
 
-const url = "http://localhost:4000/products"
+const url = "https://fresh-prince.herokuapp.com/products"
 const template = document.getElementById('cardsTemplate').content
 const container = document.getElementById('CardsContainer')
 const frag = document.createDocumentFragment()
+let GlobalData
 
 window.addEventListener('DOMContentLoaded', async()=>{
     let res = await fetch(url)
     const data = await res.json()
+    GlobalData = data
     getData(data)
 })
 
@@ -15,9 +17,17 @@ let getData = (data)=>{
         template.querySelector('img').setAttribute('src', images[0])
         template.querySelector('h2').textContent = name
         template.querySelector('span').textContent = `Price: $${price}.00`
-        template.querySelector('#boton').addEventListener('click', ()=> {console.log("a")})
+        template.querySelector('button').setAttribute('id', id)
         let node = template.cloneNode(true)
         frag.appendChild(node)
     });
     container.appendChild(frag)
 }
+
+window.addEventListener('click', ({target})=>{
+        if(target.classList.contains('importantButon')){
+            console.log("A");
+            localStorage.setItem('section', target.id)
+            window.location = "../pages/especification.html"
+        }
+})
